c38dc0efac98773a92a3c106ebc6b5d2e103e72f
sjoensen <sjurdur.joensen@gmail.com>
2023-06-08 12:26:48 +0200

    Added SQL script running and CSV loading

--- src/db/util.py
+++ src/db/util.py
@@ -1,10 +1,37 @@
 import os
+import pandas as pd
+import psycopg2
+import numpy as np
+from dotenv import load_dotenv
+from psycopg2.extensions import register_adapter, AsIs
+
+CSV_SEPARATOR = ','
+
+# [ filename ]
+SCHEMA_FILES = [
+    "drop_db",
+    "lines",
+    "stations",
+    "station_lines",
+    "amenities",
+    "tags",
+    "amenity_tags"
+]
+
+# [ filename, [ column_names ] ]
+CSV_FILES = [
+    ("lines", ["name", "length"]),
+    ("stations", ["name", "address", "area"]),
+    ("station_lines", ["station", "line", "position"]),
+    ("tags", ["type"])
+]
+
+ABSOLUTE_PATH = os.path.dirname(__file__)
+SCHEMA_DIRECTORY = "schema"
+DATA_DIRECTORY = "data"
 
 
 def connect():
-    import psycopg2
-    from dotenv import load_dotenv
-
     load_dotenv()
 
     conn = psycopg2.connect(
@@ -17,23 +44,58 @@ def connect():
     return conn
 
 
-def reset(connection):
-    absolute_path = os.path.dirname(__file__)
-    directory = "schema"
-
-    def get_file_string(file_name):
-        target = directory + "/" + file_name
-        return os.path.join(absolute_path, target)
+def reset() -> None:
+    _register_adapters()
 
     conn = connect()
+
     with conn.cursor() as cur:
-        with open(get_file_string("reset.sql")) as file:
-            cur.execute(file.read())
-        with open(get_file_string("tests.sql")) as file:
-            cur.execute(file.read())
+        for name in SCHEMA_FILES:
+            with open(_get_schema_file_string(name + ".sql")) as file:
+                cur.execute(file.read())
+
+    with conn.cursor() as cur:
+        for csv in CSV_FILES:
+            data = list(
+                map(lambda x: tuple(x),
+                    pd.read_csv(_get_data_file_string(csv[0] + ".csv"), sep=CSV_SEPARATOR)[csv[1]].to_records(index=False))
+                )
+            args_str = CSV_SEPARATOR.join(cur.mogrify(_get_csv_symbols(len(csv[1])), d).decode('utf-8') for d in data)
+            cur.execute("INSERT INTO " + csv[0] + " (" + (CSV_SEPARATOR.join(map(str, csv[1]))) + ") VALUES " + args_str)
+
     conn.commit()
     conn.close()
 
 
-def get_secret_key():
-    os.getenv('SECRET_KEY')
+def get_secret_key() -> str:
+    return os.getenv('SECRET_KEY')
+
+
+def _get_schema_file_string(file_name: str) -> str:
+    target = SCHEMA_DIRECTORY + "/" + file_name
+    return os.path.join(ABSOLUTE_PATH, target)
+
+
+def _get_data_file_string(file_name: str) -> str:
+    target = DATA_DIRECTORY + "/" + file_name
+    return os.path.join(ABSOLUTE_PATH, target)
+
+
+def _get_csv_symbols(n: int) -> str:
+    res = "("
+    for _ in range(n):
+        res += "%s, "
+    return (res[:-2]) + ")"
+
+
+def _adapt_numpy_float64(numpy_float64):
+    return AsIs(numpy_float64)
+
+
+def _adapt_numpy_int64(numpy_int64):
+    return AsIs(numpy_int64)
+
+
+def _register_adapters() -> None:
+    register_adapter(np.float64, _adapt_numpy_float64)
+    register_adapter(np.int64, _adapt_numpy_int64)
